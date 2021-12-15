1d77503c16d8c5d74ba8c349025620680474d2b0
karen dominguez <kdominguez@devbot.com>
2021-12-15 17:47:58 +0000

    change  output file

--- outputs.tf
+++ outputs.tf
@@ -1,44 +1,44 @@
 # ------------------------------------------------------------------------------
-## OUTPUTS
-## ------------------------------------------------------------------------------
-#output "rg_name" {
-#  value       = azurerm_resource_group.resourcegroup.name
-#  description = "Resource Group Name"
-#}
-#
-#
-#output "rg_id" {
-#  value       = azurerm_resource_group.resourcegroup.id
-#  description = "Resource Group ID"
-#}
-#
-#output "vnet_name" {
-#  value       = azurerm_virtual_network.net.name
-#  description = "vNet Name"
-#}
-#
-#output "vnet_id" {
-#  value       = azurerm_virtual_network.net.id
-#  description = "vNet ID"
-#}
-#
-#output "subnet_name" {
-#  value       = azurerm_subnet.net.name
-#  description = "Subnet name"
-#}
-#
-##output "subnet_id" {
-##  value       = azurerm_subnet.net.id
-##  description = "Subnet ID"
-#}
+# OUTPUTS
+# ------------------------------------------------------------------------------
+output "rg_name" {
+  value       = data.azurerm_resource_group.resourcegroup.name
+  description = "Resource Group Name"
+}
 
-#output "virtual_machine_name" {
-#  value       = azurerm_linux_virtual_machine.main.name
-#  description = "Linux VM name"
-#}
 
-#output "virtual_machine_id" {
-#  value       = azurerm_linux_virtual_machine.main.id
-#  description = "Linux VM ID"
-#}
+output "rg_id" {
+  value       = data.azurerm_resource_group.resourcegroup.id
+  description = "Resource Group ID"
+}
+
+output "vnet_name" {
+  value       = data.azurerm_virtual_network.net.name
+  description = "vNet Name"
+}
+
+output "vnet_id" {
+  value       = data.azurerm_virtual_network.net.id
+  description = "vNet ID"
+}
+
+output "subnet_name" {
+  value       = data.azurerm_subnet.net.name
+  description = "Subnet name"
+}
+
+output "subnet_id" {
+  value       = data.azurerm_subnet.net.id
+  description = "Subnet ID"
+}
+
+output "virtual_machine_name" {
+  value       = azurerm_linux_virtual_machine.main.name
+  description = "Linux VM name"
+}
+
+output "virtual_machine_id" {
+  value       = azurerm_linux_virtual_machine.main.id
+  description = "Linux VM ID"
+}
 
