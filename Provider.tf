702fa3cc8534ed627a13e41e29a7f2aaedc84013
Giridhar Kolli <Giridhar.Kolli@lvs1.com>
2023-07-11 09:46:30 -0700

    updated provider.tf

--- Provider.tf
+++ Provider.tf
@@ -13,6 +13,8 @@ terraform {
 provider "azurerm" {
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 
   features {
     key_vault {
